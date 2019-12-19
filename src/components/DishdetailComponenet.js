a3e17af7dcd457ecd86b97ea65b1d61b77135746
JChase2 <mr.jdchase@gmail.com>
2019-12-19 17:44:30 -0500

    Added redux-thunk and a loding component.

--- src/components/DishdetailComponenet.js
+++ src/components/DishdetailComponenet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, BreadcrumbItem, Breadcrumb, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 function RenderDish({ dish }) {
     return (
@@ -113,7 +114,6 @@ function RenderComments({ comments, addComment, dishId }) {
                         <li key={comment.id}>
                             <p>{comment.comment}</p>
                             <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-                            {console.log("The Author: " + comment.author)}
                         </li>
                     )
                 })}
@@ -126,7 +126,25 @@ function RenderComments({ comments, addComment, dishId }) {
 
 
 const DishDetail = (props) => {
-    if (props.dish != null) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
         return (
             <div className="container">
                 <div className="row">
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,30 +1,50 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
-    function RenderMenuItem ({dish, onClick}) {
-        return (
-            <Card>
-                <Link to={`/menu/${dish.id}`}>
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card>
+            <Link to={`/menu/${dish.id}`}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
-                </Link>
-            </Card>
+            </Link>
+        </Card>
+    );
+}
+
+const Menu = (props) => {
+
+    const menu = props.dishes.dishes.map((dish) => {
+        return (
+            <div className="col-12 col-md-5 m-1" key={dish.id}>
+                <RenderMenuItem dish={dish} />
+            </div>
+        );
+    });
+
+    if (props.dishes.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
         );
     }
-
-    const Menu = (props) => {
-
-        const menu = props.dishes.map((dish) => {
-            return (
-                <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish}/>
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.dishes.errMess}</h4>
                 </div>
-            );
-        });
-
+            </div>
+        );
+    }
+    else {
         return (
             <div className="container">
                 <div className="row">
@@ -43,5 +63,5 @@ import { Link } from 'react-router-dom';
             </div>
         );
     }
-
-export default Menu;
+}
+    export default Menu;
