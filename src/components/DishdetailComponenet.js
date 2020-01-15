1f2133521d6340ce82b75486d43f14c3193dedbd
JChase2 <mr.jdchase@gmail.com>
2020-01-15 00:16:19 -0500

    Am now using a json-server to host images and using fetch to load them into the app.

--- src/components/DishdetailComponenet.js
+++ src/components/DishdetailComponenet.js
@@ -3,11 +3,12 @@ import { Card, CardImg, CardText, CardBody, CardTitle, BreadcrumbItem, Breadcrum
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseURL } from '../shared/baseURL';
 
 function RenderDish({ dish }) {
     return (
         <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
+            <CardImg top src={baseURL + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
